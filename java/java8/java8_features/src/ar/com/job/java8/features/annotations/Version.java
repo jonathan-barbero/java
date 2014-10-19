abf01e4e127353a4cbfb4dbe2e09ae7ae7e578bb
jonathan-barbero <jonathan.barbero@gmail.com>
2014-10-19 00:16:53 -0300

    Merged

--- java/java8/java8_features/src/ar/com/job/java8/features/annotations/Version.java
+++ java/java8/java8_features/src/ar/com/job/java8/features/annotations/Version.java
@@ -1,13 +1,12 @@
 package ar.com.job.java8.features.annotations;
 
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
-import java.lang.annotation.ElementType;
-
 @ClassType(role = MVC.MODEL)
 @Retention(RetentionPolicy.RUNTIME)
-@java.lang.annotation.Target({ElementType.TYPE})
+@java.lang.annotation.Target({ ElementType.TYPE })
 public @interface Version {
 
 	int major();
